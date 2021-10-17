95de6c56f7160a874c709e56494971e001923e41
Lê Hoàng Anh <anhlh@blockchaindeveloper.asia>
2021-10-17 10:37:45 +0700

    Remove unused functions

--- contracts/pawn/pawn-p2p-v2/PawnLib.sol
+++ contracts/pawn/pawn-p2p-v2/PawnLib.sol
@@ -245,48 +245,18 @@ library PawnLib {
         }
     }
 
-    function add(uint256 a, uint256 b) internal pure returns (uint256) {
-        uint256 c = a + b;
-        require(c >= a, "0");//SafeMath: addition overflow
+    function checkLenderAccount(
+        uint256 loanAmount, 
+        address loanToken, 
+        address owner, 
+        address spender
+    ) internal view {
+        // Check if lender has enough balance and allowance for lending
+        uint256 lenderCurrentBalance = IERC20Upgradeable(loanToken).balanceOf(owner);
+        require(lenderCurrentBalance >= loanAmount, "4"); // insufficient balance
 
-        return c;
-    }
-
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        return sub(a, b, "1"); //SafeMath: subtraction overflow
-    }
-
-    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
-        require(b <= a, errorMessage);
-        uint256 c = a - b;
-
-        return c;
-    }
-
-    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
-        // benefit is lost if 'b' is also tested.
-        // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
-        if (a == 0) {
-            return 0;
-        }
-
-        uint256 c = a * b;
-        require(c / a == b, "2"); //SafeMath: multiplication overflow
-
-        return c;
-    }
-
-    function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        return div(a, b, "3");  //SafeMath: division by zero
-    }
-
-    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
-        require(b > 0, errorMessage);
-        uint256 c = a / b;
-        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-
-        return c;
+        uint256 lenderCurrentAllowance = IERC20Upgradeable(loanToken).allowance(owner, spender);
+        require(lenderCurrentAllowance >= loanAmount, "5"); // allowance not enough
     }
 
 }
