c6fe4d8e586d7e6ac4233f7dbe561bd9274d5edf
Lê Hoàng Anh <anhlh@blockchaindeveloper.asia>
2021-11-18 14:00:13 +0700

    Update Hub contract

---
+++ contracts/pawn/hub/HubInterface.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+interface HubInterface {
+    struct SystemConfig {
+        address systemFeeWallet;
+        address systemFeeToken;
+    }
+
+    struct PawnConfig {
+        uint256 ZOOM;
+        uint256 systemFeeRate;
+        uint256 penaltyRate;
+        uint256 prepaidFeeRate;
+        uint256 lateThreshold;
+        mapping(address => uint256) whitelistedCollateral;
+    }
+
+    // struct PawnNFTConfig {}
+}
