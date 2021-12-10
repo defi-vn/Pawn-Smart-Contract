e7e1cdf6cce1f5b38ba04d84026455c312684b96
aquangvm <quangvm@blockchaindeveloper.asia>
2021-12-10 16:04:33 +0700

    update data

--- scripts/2_deploy_Exchange.js
+++ scripts/2_deploy_Exchange.js
@@ -26,12 +26,23 @@ async function main() {
     const ExchangeContract  = await hre.upgrades.deployProxy(ExchangeFactory,[HubProxyAddr], proxyType);
 
     await ExchangeContract.deployed();
+    const signature = await ExchangeContract.signature();
 
     console.log(`EXCHANGE_CONTRACT_ADDRESS: ${ExchangeContract.address}`);
+    console.log(`Signature: \x1b[36m${signature}\x1b[0m`);
 
     implementationAddress = await hre.upgrades.erc1967.getImplementationAddress(ExchangeContract.address);
     console.log(`${ExchangeArtifact.contractName} implementation address: ${implementationAddress}`);
 
+    const HubFactory = await hre.ethers.getContractFactory(HubBuildName);
+    const HubArtifact = await hre.artifacts.readArtifact(HubBuildName);
+    const HubContract = HubFactory.attach(HubProxyAddr);
+
+
+    console.log(`HUB_ADDRESS: \x1b[31m${HubContract.address}\x1b[0m`);
+    console.log(`Registering \x1b[36m${ExchangeArtifact.contractName}\x1b[0m to \x1b[31m${HubArtifact.contractName}\x1b[0m...`);
+
+    await HubContract.registerContract(signature,ExchangeContract.address,ExchangeArtifact.contractName);
     console.log("============================================================\n\r");
 }
   
